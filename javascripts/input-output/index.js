e52364fc3c6a820cb9ad99e5be6ebcec2c7237a3
mullajonovhikmatullo <hikmatullo.mullajonov@gmail.com>
2026-02-21 01:17:29 +0500

    Initialize project with basic input-output JavaScript exercises and initial HTML structure

---
+++ javascripts/input-output/index.js
@@ -0,0 +1,65 @@
+// ============================== Begin 1 ==============================
+// let squareLenght = +prompt("Enter the square lenght: ");
+// let squareArea = squareLenght * 4;
+// console.log(squareArea);
+
+// ============================== Begin 2 ==============================
+// let squareLenght = +prompt("Enter the square lenght: ");
+// let squareArea = squareLenght * squareLenght;
+// console.log(squareArea);
+
+// ============================== Begin 3 ==============================
+// let rectangleWidth = +prompt("Enter the rectangle width: ");
+// let rectangleHeight = +prompt("Enter the rectangle height: ");
+// let rectangleArea = rectangleWidth * rectangleHeight;
+// let rectanglePerimeter = rectangleWidth * 2 + rectangleHeight * 2;
+// console.log(rectangleArea, rectanglePerimeter);
+
+// ============================== Begin 4 ==============================
+// const PI = 3.14;
+// let circleDiameter = +prompt("Enter the circle diameter: ");
+// let circleLength =  circleDiameter * PI;
+// console.log(circleLength);
+
+// ============================== Begin 5 ==============================
+// let cubicLenght = +prompt("Enter the kubik lenght: ");
+// let cubicVolume = cubicLenght * cubicLenght * cubicLenght;
+// let cubicArea = cubicLenght * cubicLenght * 6;
+// console.log(cubicArea, cubicVolume);
+
+// ============================== Begin 6 ==============================
+// let parallellopipedA = +prompt("Enter the first side of the parallelogram: ");
+// let parallellopipedB = +prompt("Enter the second side of the parallelogram: ");
+// let parallellopipedC = +prompt("Enter the height of the parallelogram: ");
+// let parallellopipedWeight = parallellopipedA * parallellopipedB * parallellopipedC;
+// let parallellopipedArea = 2 * (parallellopipedA * parallellopipedB + parallellopipedB * parallellopipedC + parallellopipedA * parallellopipedC);
+// console.log(parallellopipedArea, parallellopipedWeight);
+
+// ============================== Begin 7 ==============================
+// let cycleRadius = +prompt("Enter the circle radius: ");
+// const PI = 3.14;
+// let cycleCircumference = 2 * PI * cycleRadius;
+// let cycleArea = PI * cycleRadius * cycleRadius;
+// console.log(cycleCircumference, cycleArea);
+
+// ============================== Begin 8 ==============================
+// let a = 20;
+// let b = 50;
+// let c = (a + b) / 2;
+// console.log(c);
+
+// ============================== Begin 9 ==============================
+// let a = 5;
+// let b = 7;
+// let c = Math.sqrt(a * b);
+// let d = Math.pow(a * b, 1/2);
+// console.log(c, d);
+
+// ============================== Begin 10 ==============================
+// let a = 10;
+// let b = 50;
+// let sum = a + b;
+// let multiply = a * b;
+// let kvA = a * a;
+// let kvB = b * b;
+// console.log(sum, multiply, kvA, kvB);
