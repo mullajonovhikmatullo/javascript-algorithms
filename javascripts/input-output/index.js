c3919c52b2eccc1c1936dae2a5c190de4f92ff39
mullajonovhikmatullo <hikmatullo.mullajonov@gmail.com>
2026-02-21 16:13:05 +0500

    Initialize additional input-output JavaScript exercises in `index.js` and update workspace settings

--- javascripts/input-output/index.js
+++ javascripts/input-output/index.js
@@ -63,3 +63,258 @@
 // let kvA = a * a;
 // let kvB = b * b;
 // console.log(sum, multiply, kvA, kvB);
+
+// ============================== Begin 11 ==============================
+// let a = 20;
+// let b = 30;
+// let sum = a + b;
+// let multiply = a * b;
+// let moduleA = Math.abs(a);
+// let moduleB = Math.abs(b);
+// console.log(sum, multiply, moduleA, moduleB);
+
+// ============================== Begin 12 ==============================
+// let a = 3;
+// let b = 4;
+// let gipotenusa = Math.sqrt(a * a + b * b);
+// let p = a + b + gipotenusa;
+// console.log(p);
+
+// ============================== Begin 13 ==============================
+// let r1 = 10;
+// let r2 = 4;
+//
+// let S1 = Math.PI * r1 * r1;
+// let S2 = Math.PI * r2 * r2;
+// let S3 = Math.PI * (r1 * r1 - r2 * r2);
+// console.log(S1, S2, S3);
+
+// ============================== Begin 14 ==============================
+// let L = 20;
+// let R = L / (2 * Math.PI);
+// let S = Math.PI * R * R;
+// console.log(S, R);
+
+// ============================== Begin 15 ==============================
+// let S = 30;
+// let R = Math.sqrt( S / Math.PI);
+// let D = 2 * R;
+// console.log(R, D);
+
+// ============================== Begin 16 ==============================
+// let x1 = 6;
+// let x2 = 18;
+// let l = Math.abs(x1 - x2);
+// console.log(l)
+
+// ============================== Begin 17 ==============================
+// let a = 10;
+// let b = 20;
+// let c = 30;
+//
+// let ac = Math.abs(a - c);
+// let bc = Math.abs(b - c);
+// let sum = ac + bc;
+// console.log(sum);
+
+// ============================== Begin 18 ==============================
+// let a = 10;
+// let c = 20;
+// let b = 30;
+//
+// let multiply = Math.abs(a - c) * Math.abs(b - c);
+// console.log(multiply);
+
+// ============================== Begin 19 ==============================
+// let x1 = 5;
+// let x2 = 17;
+// let y1 = 9;
+// let y2 = 11;
+//
+// let a = Math.abs(x2 - x1);
+// let b = Math.abs(y2 - y1);
+//
+// let perimeter = (a + b) * 2;
+// let area = a * b;
+//
+// console.log(perimeter, area);
+
+// ============================== Begin 20 ==============================
+
+// let x1 = 10;
+// let x2 = 20;
+// let y1 = 10;
+// let y2 = 20;
+//
+// let a = Math.abs(x2 - x1);
+// let b = Math.abs(y2 - y1);
+//
+// let l = Math.sqrt(a * a + b * b);
+// console.log(l);
+
+// ============================== Begin 21 ==============================
+// let x1 = 2;
+// let x2 = 10;
+// let x3 = 7;
+// let y1 = 2;
+// let y2 = 3;
+// let y3 = 12;
+//
+// let a = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+// let b = Math.sqrt(Math.pow(x3 - x1, 2) + Math.pow(y3 - y1, 2));
+// let c = Math.sqrt(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2));
+//
+// let p = (a + b + c) / 2;
+// let S = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+// console.log(S);
+
+// ============================== Begin 22 ==============================
+// let a = 55;
+// let b = 111;
+// let c = b;
+// b = a;
+// a = c;
+// console.log("a = " + a, "b = " + b);
+
+// ============================== Begin 23 ==============================
+// let a = 13;
+// let b = 17;
+// let c = 19;
+//
+// let d = a;
+// a = b;
+// b = c;
+// c = d;
+//
+// console.log('a = ' + a,'b = ' + b,'c = ' + c);
+
+// ============================== Begin 24 ==============================
+// let a = 13;
+// let b = 17;
+// let c = 19;
+//
+// let d = a;
+// a = c;
+// c = b;
+// b = d;
+// console.log('a = ' + a,'b = ' + b,'c = ' + c);
+
+// ============================== Begin 25 ==============================
+// let x = 2;
+// let y = 3 * Math.pow(x, 6) - 6 * x * x - 7;
+// console.log(y);
+
+// ============================== Begin 26 ==============================
+// let x = 1;
+// let y = 4 * Math.pow(x - 3, 6) - 7 * Math.pow(x - 3, 3) + 2;
+// console.log(y);
+
+// ============================== Begin 27 ==============================
+// let A = 2;
+// console.log(Math.pow(A, 2), Math.pow(A, 4), Math.pow(A, 8));
+
+// ============================== Begin 28 ==============================
+// let A = 1;
+// console.log(Math.pow(A, 2), Math.pow(A, 3), Math.pow(A, 5), Math.pow(A, 10), Math.pow(A, 15));
+
+// ============================== Begin 29 ==============================
+// let alfa = 190;
+// let radian = alfa * Math.PI / 180;
+// console.log(radian);
+
+// ============================== Begin 30 ==============================
+// let radian = 1.57;
+// let alfa = radian * 180 / Math.PI;
+// console.log(alfa);
+
+// ============================== Begin 31 ==============================
+// let Farengeyt = 19;
+// let Celsius = (Farengeyt - 32) * 5 / 9;
+// console.log(Celsius);
+
+// ============================== Begin 32 ==============================
+// let Celsius = 10;
+// let Farengeyt = Celsius * 9 / 5 + 32;
+// console.log(Farengeyt);
+
+// ============================== Begin 33 ==============================
+// let X = 5;
+// let A = 55000;
+// let oneKg = A / X;
+//
+// let yKg = 4;
+// let yKgSum = yKg * oneKg;
+//
+// console.log(yKgSum);
+
+// ============================== Begin 34 ==============================
+// let x = 4;
+// let A = 2000;
+// let y = 2;
+// let B = 500;
+//
+// let diff = A / x - B / y;
+// console.log(diff);
+
+// ============================== Begin 35 ==============================
+// let V = 50;
+// let U = 10;
+//
+// let T1 = 2;
+// let T2 = 1.5;
+//
+// console.log((V + U) * T1);
+// console.log((V - U) * T2);
+
+// ============================== Begin 36 ==============================
+// let V1 = 50;
+// let V2 = 70;
+// let S = 3;
+// let T = 1;
+//
+// let L = V1 * T + V2 * T + S;
+//
+// console.log(L);
+
+// ============================== Begin 37 ==============================
+// let V1 = 50;
+// let V2 = 70;
+// let S = 200;
+// let T = 1;
+//
+// let L = S - (V1 + V2) * T;
+//
+// console.log(L);
+
+// ============================== Begin 38 ==============================
+// let A = 6;
+// let B = 9;
+//
+// let x = - (B / A);
+//
+// console.log(x);
+
+// ============================== Begin 39 ==============================
+// let A = 1;
+// let B = 6;
+// let C = 5;
+//
+// let D = B * B - 4 * A * C;
+// let X1 = (-B + Math.sqrt(D)) / (2 * A);
+// let X2 = (-B - Math.sqrt(D)) / (2 * A);
+//
+// console.log(X1, X2);
+
+// ============================== Begin 40 ==============================
+// let A1 = 1;
+// let B1 = 2;
+// let C1 = 3;
+// let A2 = 2;
+// let B2 = 3;
+// let C2 = 4;
+//
+// let D = A1 * B2 - A2 * B1;
+// let X = (C1 * B2 - C2 * B1) / D;
+// let Y = (A1 * C2 - A2 * C1) / D;
+//
+// console.log(X, Y);
