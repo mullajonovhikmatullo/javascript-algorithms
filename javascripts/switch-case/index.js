8f3833b06e77ebdebf09682b711d5aac2b8c4599
mullajonovhikmatullo <hikmatullo.mullajonov@gmail.com>
2026-02-24 19:55:16 +0500

    Added switch case from 1 to 12 solutions.

---
+++ javascripts/switch-case/index.js
@@ -0,0 +1,383 @@
+// =============== Switch case 1 ===============
+// let son = 7;
+//
+// switch (son) {
+//     case 1:
+//         console.log('Dushanba'); break;
+//     case 2:
+//         console.log('Seshanba'); break;
+//     case 3:
+//         console.log('Chorshanba'); break;
+//     case 4:
+//         console.log('Payshanba'); break;
+//     case 5:
+//         console.log('Juma'); break;
+//     case 6:
+//         console.log('Shanba'); break;
+//     case 7:
+//         console.log('Yakshanba'); break;
+//     default:
+//         console.log('Bunday hafta kuni mavjud emas !');
+// }
+
+// =============== Switch case 2 ===============
+// let son = 3;
+//
+// switch (son) {
+//     case 1:
+//         console.log('Yomon'); break;
+//     case 2:
+//         console.log('Qoniqarsiz'); break;
+//     case 3:
+//         console.log('Qoniqarli'); break;
+//     case 4:
+//         console.log('Yaxshi'); break;
+//     case 5:
+//         console.log('A\'lo!'); break;
+//     default:
+//         console.log('Bunday hafta kuni mavjud emas !');
+// }
+
+// =============== Switch case 3 ===============
+// let month = 3;
+//
+// switch (month) {
+//     case 3:
+//     case 4:
+//     case 5:
+//         console.log('Bahor'); break;
+//     case 6:
+//     case 7:
+//     case 8:
+//         console.log('Yoz'); break;
+//     case 9:
+//     case 10:
+//     case 11:
+//         console.log('Kuz'); break;
+//     case 12:
+//     case 1:
+//     case 2:
+//         console.log('Qish'); break;
+//     default:
+//         console.log('Bunday oy mavjud emas !');
+// }
+
+// =============== Switch case 4 ===============
+// let month = 2;
+//
+// switch (month) {
+//     case 1:
+//     case 3:
+//     case 5:
+//     case 7:
+//     case 8:
+//     case 10:
+//     case 12:
+//         console.log(month + '-oyda 31 kun bor'); break;
+//     case 4:
+//     case 6:
+//     case 9:
+//     case 11:
+//         console.log(month + '-oyda 30 kun bor'); break;
+//     case 2:
+//         console.log(month + '-oyda 28 kun bor'); break;
+//     default:
+//         console.log('Bunday oy mavjud emas !');
+// }
+
+// =============== Switch case 5 ===============
+// let A = 21;
+// let B = 33;
+// let amal = 3;
+// let result;
+//
+// switch (amal) {
+//     case 1: result = A + B; break;
+//     case 2: result = A - B; break;
+//     case 3: result = A / B; break;
+//     case 4: result = A * B; break;
+//     default:
+//         console.log('Bunday amalni hali bajara olmayman !');
+// }
+// console.log(result);
+
+// =============== Switch case 6 ===============
+// let birlik = 3;
+// let uzunlik = 10;
+// let natija = uzunlik;
+//
+// switch (birlik) {
+//     case 1: natija = uzunlik / 10; break;
+//     case 2: natija = uzunlik * 1000; break;
+//     case 3: break;
+//     case 4: natija = uzunlik / 1000; break;
+//     case 5: natija = uzunlik / 100; break;
+//     default:
+//         console.log('Bu birlikni tanimadim !');
+// }
+// console.log(natija);
+
+
+// =============== Switch case 7 ===============
+// let birlik = 3;
+// let weight = 10;
+// let natija = weight;
+//
+// switch (birlik) {
+//     case 1: break;
+//     case 2: natija = weight / 1_000_000; break;
+//     case 3: natija = weight / 1000; break;
+//     case 4: natija = weight * 1000; break;
+//     case 5: natija = weight * 100; break;
+//     default:
+//         console.log('Bu birlikni tanimadim !');
+// }
+// console.log(natija);
+
+// =============== Switch case 8 ===============
+// let D = 25;
+// let M = 2;
+//
+// switch (M) {
+//     case 1:
+//         console.log(D + ' - yanvar'); break;
+//     case 2:
+//         console.log(D + ' - fevral'); break;
+//     case 3:
+//         console.log(D + ' - mart'); break;
+//     case 4:
+//         console.log(D + ' - aprel'); break;
+//     case 5:
+//         console.log(D + ' - may'); break;
+//     case 6:
+//         console.log(D + ' - iyun'); break;
+//     case 7:
+//         console.log(D + ' - iyul'); break;
+//     case 8:
+//         console.log(D + ' - avgust'); break;
+//     case 9:
+//         console.log(D + ' - sentabr'); break;
+//     case 10:
+//         console.log(D + ' - oktabr'); break;
+//     case 11:
+//         console.log(D + ' - noyabr'); break;
+//     case 12:
+//         console.log(D + ' - dekabr'); break;
+//     default:
+//         console.log('Bunday oy mavjud emas !');
+// }
+
+// =============== Switch case 9 ===============
+// let D = 20;
+// let M = 2;
+//
+// D = D + 1;
+//
+// switch (M) {
+//     case 1:
+//         console.log(D + ' - yanvar'); break;
+//     case 2:
+//         console.log(D + ' - fevral'); break;
+//     case 3:
+//         console.log(D + ' - mart'); break;
+//     case 4:
+//         console.log(D + ' - aprel'); break;
+//     case 5:
+//         console.log(D + ' - may'); break;
+//     case 6:
+//         console.log(D + ' - iyun'); break;
+//     case 7:
+//         console.log(D + ' - iyul'); break;
+//     case 8:
+//         console.log(D + ' - avgust'); break;
+//     case 9:
+//         console.log(D + ' - sentabr'); break;
+//     case 10:
+//         console.log(D + ' - oktabr'); break;
+//     case 11:
+//         console.log(D + ' - noyabr'); break;
+//     case 12:
+//         console.log(D + ' - dekabr'); break;
+//     default:
+//         console.log('Bunday oy mavjud emas !');
+// }
+
+// =============== Switch case 10 ===============
+// let Y = 'q';   // s(shimol), q(sharq), j(janub), g(g'arb) yo'nalish.
+// let K = 1;   // 0 to'g'riga, 1 chapga, 2 o'ngga komandalari
+//
+// switch (Y) {
+//     case 's': {
+//         switch (K) {
+//             case 0:
+//                 console.log('Shimol'); break;
+//             case 1:
+//                 console.log('G\'arb'); break;
+//             case 2:
+//                 console.log('Sharq'); break;
+//         }
+//         break;
+//     }
+//     case 'q': {
+//         switch (K) {
+//             case 0:
+//                 console.log('Sharq'); break;
+//             case 1:
+//                 console.log('Shimol'); break;
+//             case 2:
+//                 console.log('Janub'); break;
+//         }
+//         break;
+//     }
+//     case 'j': {
+//         switch (K) {
+//             case 0:
+//                 console.log('Janub'); break;
+//             case 1:
+//                 console.log('Sharq'); break;
+//             case 2:
+//                 console.log('G\'arb'); break;
+//         }
+//         break;
+//     }
+//     case 'g': {
+//         switch (K) {
+//             case 0:
+//                 console.log('G\'arb'); break;
+//             case 1:
+//                 console.log('Janub'); break;
+//             case 2:
+//                 console.log('Shimol'); break;
+//         }
+//         break;
+//     }
+// }
+
+// switch (true) {
+//     case Y === 's' && K === 0: console.log('Shimol'); break;
+//     case Y === 's' && K === 1: console.log('G\'arb'); break;
+//     case Y === 's' && K === 2: console.log('Sharq'); break;
+//
+//     case Y === 'q' && K === 0: console.log('Sharq'); break;
+//     case Y === 'q' && K === 1: console.log('Shimol'); break;
+//     case Y === 'q' && K === 2: console.log('Janub'); break;
+//
+//     case Y === 'j' && K === 0: console.log('Janub'); break;
+//     case Y === 'j' && K === 1: console.log('Sharq'); break;
+//     case Y === 'j' && K === 2: console.log('G\'arb'); break;
+//
+//     case Y === 'g' && K === 0: console.log('G\'arb'); break;
+//     case Y === 'g' && K === 1: console.log('Janub'); break;
+//     case Y === 'g' && K === 2: console.log('Shimol'); break;
+// }
+
+// =============== Switch case 11 ===============
+// let Y = 's';  // 's', 'j', 'g', 'q'
+// let K1 = 0;   // 0, 1, 2
+// let K2 = 2;   // 0, 1, 2
+//
+// switch (true) {
+//     // Janub chiquvchi holatlar
+//     case Y === 's' && K1 === 1 && K2 === 1:
+//     case Y === 'q' && K1 === 1 && K2 === 2:
+//     case Y === 'q' && K1 === 2 && K2 === 1:
+//     case Y === 'j' && K1 === 0 && K2 === 1:
+//     case Y === 'j' && K1 === 1 && K2 === 0:
+//     case Y === 'j' && K1 === 2 && K2 === 2:
+//     case Y === 'g' && K1 === 0 && K2 === 2:
+//     case Y === 'g' && K1 === 2 && K2 === 0:
+//     case Y === 's' && K1 === 0 && K2 === 0: console.log('Janub'); break;
+//
+//     // Shimol chiquvchi holatlar
+//     case Y === 's' && K1 === 1 && K2 === 0:
+//     case Y === 's' && K1 === 2 && K2 === 2:
+//     case Y === 'q' && K1 === 0 && K2 === 2:
+//     case Y === 'q' && K1 === 2 && K2 === 0:
+//     case Y === 'j' && K1 === 0 && K2 === 0:
+//     case Y === 'j' && K1 === 1 && K2 === 1:
+//     case Y === 'g' && K1 === 1 && K2 === 2:
+//     case Y === 'g' && K1 === 2 && K2 === 1:
+//     case Y === 's' && K1 === 0 && K2 === 1: console.log('Shimol'); break;
+//
+//     // Garb chiquvchi holatlar
+//     case Y === 's' && K1 === 2 && K2 === 0:
+//     case Y === 'q' && K1 === 0 && K2 === 0:
+//     case Y === 'j' && K1 === 1 && K2 === 2:
+//     case Y === 'j' && K1 === 2 && K2 === 1:
+//     case Y === 'g' && K1 === 0 && K2 === 1:
+//     case Y === 'g' && K1 === 1 && K2 === 0:
+//     case Y === 'g' && K1 === 2 && K2 === 2:
+//     case Y === 'q' && K1 === 1 && K2 === 1:
+//     case Y === 's' && K1 === 0 && K2 === 2: console.log('Garb'); break;
+//
+//     // Sharq chiquvchi holatlar.
+//     case Y === 's' && K1 === 2 && K2 === 1:
+//     case Y === 'q' && K1 === 0 && K2 === 1:
+//     case Y === 'q' && K1 === 1 && K2 === 0:
+//     case Y === 'q' && K1 === 2 && K2 === 2:
+//     case Y === 'j' && K1 === 0 && K2 === 2:
+//     case Y === 'j' && K1 === 2 && K2 === 0:
+//     case Y === 'g' && K1 === 0 && K2 === 0:
+//     case Y === 'g' && K1 === 1 && K2 === 1:
+//     case Y === 's' && K1 === 1 && K2 === 2: console.log('Sharq'); break;
+//
+//     default:
+//         console.log('Yo\'nalish aniqlanmadi !');
+// }
+
+// =============== Switch case 12 ===============
+// let element = 20;
+// let order = 2;
+//
+// switch (order) {
+//     case 1: {
+//         console.log('D = ' + 2 * element);
+//         console.log('L = ' + 2 * Math.PI * element);
+//         console.log('S = ' + Math.PI * element * element);
+//         break;
+//     }
+//     case 2: {
+//         console.log('R = ' + element / 2);
+//         console.log('L = ' + Math.PI * element);
+//         console.log('S = ' + Math.PI * element * element / 4);
+//         break;
+//     }
+//     case 3: {
+//         console.log('R = ' + element / 2 * Math.PI);
+//         console.log('D = ' + element / Math.PI);
+//         console.log('S = ' + element * element / 4 * Math.PI);
+//         break;
+//     }
+//     case 4: {
+//         console.log('R = ' + Math.sqrt(element / Math.PI));
+//         console.log('D = ' + 2 * Math.sqrt(element / Math.PI));
+//         console.log('L = ' + 2 * Math.PI * Math.sqrt(element / Math.PI));
+//         break;
+//     }
+//     default:
+//         console.log("Something went wrong !");
+// }
+
+
+// =============== Switch case 13 ===============
+
+
+// =============== Switch case 14 ===============
+
+
+// =============== Switch case 15 ===============
+
+
+// =============== Switch case 16 ===============
+
+
+// =============== Switch case 17 ===============
+
+
+// =============== Switch case 18 ===============
+
+
+// =============== Switch case 19 ===============
+
+
+// =============== Switch case 20 ===============
