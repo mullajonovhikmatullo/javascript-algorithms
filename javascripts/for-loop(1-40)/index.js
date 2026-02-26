67e6ba3ea4fe642c451c157d54cd7a87916fce6a
mullajonovhikmatullo <hikmatullo.mullajonov@gmail.com>
2026-02-27 00:15:15 +0500

    Added switch case exercises 13-20 and initiated for-loop 1-40 solutions

---
+++ javascripts/for-loop(1-40)/index.js
@@ -0,0 +1,198 @@
+// =============== For loop 1 ===============
+// let k = 3;
+// let n = 5;
+//
+// for(let i = 0; i < n; i ++) {
+//     console.log(k);
+// }
+
+// =============== For loop 2 ===============
+// let a = 5;
+// let b = 20;
+//
+// for(let i = a; i <= b; i++) {
+//     console.log(i);
+// }
+
+// =============== For loop 3 ===============
+// let a = 5;
+// let b = 20;
+// let count = 0;
+//
+// for(let i = b; b > a; b --) {
+//     count ++;
+//     console.log(i);
+// }
+// console.log(count);
+
+// =============== For loop 4 ===============
+// let narx = 17000;
+// for(let i = 1; i <= 10; i++) {
+//     console.log(i + ' kg konfet narxi = ' + narx * i);
+// }
+
+// =============== For loop 5 ===============
+// let narx = 17000;
+// for(let i = 0.1; i <= 1; i += 0.1) {
+//     console.log(i + ' kg konfet narxi = ' + Math.round(narx * i));
+// }
+
+// =============== For loop 6 ===============
+// let narx = 17000;
+// for(let i = 1.2; i <= 2; i += 0.2) {
+//     console.log(i + ' kg konfet narxi = ' + Math.round(narx * i));
+// }
+
+// =============== For loop 7 ===============
+// let a = 1;
+// let b = 5;
+// let sum = 0;
+//
+// for(let i = a + 1; i < b; i += 1) {
+//     i = Math.floor(i);
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+// =============== For loop 8 ===============
+// let a = 1;
+// let b = 5;
+// let multiply = 1;
+//
+// for(let i = a; i < b; i += 1) {
+//     i = Math.floor(i);
+//     multiply = multiply * i;
+// }
+// console.log(multiply);
+
+// =============== For loop 9 ===============
+// let a = 1;
+// let b = 5;
+// let multiply = 0;
+//
+// for(let i = a; i <= b; i += 1) {
+//     i = Math.floor(i);
+//     multiply = multiply + i * i;
+// }
+// console.log(multiply);
+
+// =============== For loop 10 ===============
+// let n = 10;
+// let sum = 0;
+//
+// for(let i = 1; i <= n; i ++) {
+//     sum = sum + 1 / i;
+// }
+// console.log(sum);
+
+// =============== For loop 11 ===============
+// let n = 10;
+// let sum = 0;
+//
+// for(let i = 0; i <= n; i ++) {
+//     sum = sum + Math.pow(n + i, 2);
+// }
+// console.log(sum);
+
+// =============== For loop 12 ===============
+// let n = 10;
+// let multiply = 1;
+// let a1 = 1.1;
+//
+// for(let i = 1; i <= n; i ++) {
+//     multiply = multiply * a1;
+//     a1 += 0.1;
+// }
+// console.log(multiply);
+
+// =============== For loop 13 ===============
+// let n = 10;
+// let sum = 0;
+// let a1 = 1.1;
+//
+// for(let i = 1; i <= n; i ++) {
+//     sum = sum + a1;
+//     a1 += 0.1;
+//     a1 *= -1;
+// }
+// console.log(sum);
+
+// =============== For loop 14 ===============
+
+
+// =============== For loop 15 ===============
+
+
+// =============== For loop 16 ===============
+
+
+// =============== For loop 17 ===============
+
+
+// =============== For loop 18 ===============
+
+
+// =============== For loop 19 ===============
+
+
+// =============== For loop 20 ===============
+
+
+// =============== For loop 21 ===============
+
+
+// =============== For loop 22 ===============
+
+
+// =============== For loop 23 ===============
+
+
+// =============== For loop 24 ===============
+
+
+// =============== For loop 25 ===============
+
+
+// =============== For loop 26 ===============
+
+
+// =============== For loop 27 ===============
+
+
+// =============== For loop 28 ===============
+
+
+// =============== For loop 29 ===============
+
+
+// =============== For loop 30 ===============
+
+
+// =============== For loop 31 ===============
+
+
+// =============== For loop 32 ===============
+
+
+// =============== For loop 33 ===============
+
+
+// =============== For loop 34 ===============
+
+
+// =============== For loop 35 ===============
+
+
+// =============== For loop 36 ===============
+
+
+// =============== For loop 37 ===============
+
+
+// =============== For loop 38 ===============
+
+
+// =============== For loop 39 ===============
+
+
+// =============== For loop 40 ===============
--- javascripts/switch-case(1-20)/index.js
+++ javascripts/switch-case(1-20)/index.js
@@ -360,24 +360,336 @@
 
 
 // =============== Switch case 13 ===============
-
+// let element = 1;
+// let a, c, h, S;
+//
+// switch (element) {
+//     case 1: {
+//         a = 5;
+//         c = Math.sqrt(2) * a;
+//         h = c / 2;
+//         S = c * h / 2;
+//         break;
+//     }
+//     case 2: {
+//         c = 3;
+//         a = c / Math.sqrt(2);
+//         h = c / 2;
+//         S = c * h / 2;
+//         break;
+//     }
+//     case 3: {
+//         h = 4;
+//         c = 2 * h;
+//         a = c / Math.sqrt(2);
+//         S = c * h / 2;
+//         break;
+//     }
+//     case 4: {
+//         S = 10;
+//         a = Math.sqrt(2 * S);
+//         c = Math.sqrt(2 * a * a);
+//         h = Math.sqrt(a * a / 2);
+//         break;
+//     }
+//     default:
+//         console.log('I dont know !');
+// }
+// console.log(S, a, c, h);
 
 // =============== Switch case 14 ===============
-
+// let amal = 1;
+// let a, R1, R2, S;
+//
+// switch (amal) {
+//     case 1: {
+//         a = 4;
+//         R1 = a * Math.sqrt(3) / 4;
+//         R2 = 2 * R1;
+//         S = a * a * Math.sqrt(3) / 4;
+//         break;
+//     }
+//     case 2: {
+//         R1 = 4;
+//         a = 6 * R1 / Math.sqrt(3);
+//         R2 = 2 * R1;
+//         S = a * a * Math.sqrt(3) / 4;
+//         break;
+//     }
+//     case 3: {
+//         R2 = 4;
+//         R1 = R2 / 2;
+//         a = 6 * R1 / Math.sqrt(3);
+//         S = a * a * Math.sqrt(3) / 4;
+//         break;
+//     }
+//     case 4: {
+//         S = 16;
+//         a = Math.sqrt(S * 4 / Math.sqrt(3));
+//         R1 = a * Math.sqrt(3) / 6;
+//         R2 = 2 * R1;
+//         break;
+//     }
+//     default:
+//         console.log('I dont know !');
+// }
+//
+// console.log(S, a, R1, R2);
 
 // =============== Switch case 15 ===============
-
+// let M = 2;  // 1 <= M <= 4;
+// let N = 7;  // 6 <= N <= 14;
+//
+// switch (true) {
+//     //  gisht
+//     case M === 1 && N === 6:
+//     case M === 1 && N === 7:
+//     case M === 1 && N === 8:
+//     case M === 1 && N === 9:
+//     case M === 1 && N === 10: console.log(N + " - g'isht"); break;
+//     case M === 1 && N === 11: console.log('valet' + " - g'isht"); break;
+//     case M === 1 && N === 12: console.log('dama' + " - g'isht"); break;
+//     case M === 1 && N === 13: console.log('karol' + " - g'isht"); break;
+//     case M === 1 && N === 14: console.log('tuz' + " - g'isht"); break;
+//     // olma
+//     case M === 2 && N === 6:
+//     case M === 2 && N === 7:
+//     case M === 2 && N === 8:
+//     case M === 2 && N === 9:
+//     case M === 2 && N === 10: console.log(N + " - olma"); break;
+//     case M === 2 && N === 11: console.log('valet' + " - olma"); break;
+//     case M === 2 && N === 12: console.log('dama' + " - olma"); break;
+//     case M === 2 && N === 13: console.log('karol' + " - olma"); break;
+//     case M === 2 && N === 14: console.log('tuz' + " - olma"); break;
+//     // chillak
+//     case M === 3 && N === 6:
+//     case M === 3 && N === 7:
+//     case M === 3 && N === 8:
+//     case M === 3 && N === 9:
+//     case M === 3 && N === 10: console.log(N + " - chillak"); break;
+//     case M === 3 && N === 11: console.log('valet' + " - chillak"); break;
+//     case M === 3 && N === 12: console.log('dama' + " - chillak"); break;
+//     case M === 3 && N === 13: console.log('karol' + " - chillak"); break;
+//     case M === 3 && N === 14: console.log('tuz' + " - chillak"); break;
+//     // qarg'a
+//     case M === 4 && N === 6:
+//     case M === 4 && N === 7:
+//     case M === 4 && N === 8:
+//     case M === 4 && N === 9:
+//     case M === 4 && N === 10: console.log(N + " - qarg'a"); break;
+//     case M === 4 && N === 11: console.log('valet' + " - qarg'a"); break;
+//     case M === 4 && N === 12: console.log('dama' + " - qarg'a"); break;
+//     case M === 4 && N === 13: console.log('karol' + " - qarg'a"); break;
+//     case M === 4 && N === 14: console.log('tuz' + " - qarg'a"); break;
+//     default:
+//         console.log('Bunday karta mavjud emas !');
+// }
 
 // =============== Switch case 16 ===============
+// let old = 26;
+//
+// if(20 <= old && old <= 69) {
+//     let oldTenth = Math.floor(old % 100 / 10);
+//     let oldOneth = old % 10;
+//     let answer;
+//     switch (oldTenth) {
+//         case 1: answer = 'O\'n '; break;
+//         case 2: answer = 'Yigirma '; break;
+//         case 3: answer = 'O\'ttiz '; break;
+//         case 4: answer = 'Qirq '; break;
+//         case 5: answer = 'Ellik '; break;
+//         case 6: answer = 'Oltmish '; break;
+//         case 7: answer = 'Yetmish '; break;
+//         case 8: answer = 'Sakson '; break;
+//         case 9: answer = 'To\'qson '; break;
+//     }
+//     switch (oldOneth) {
+//         case 1: answer += 'bir'; break;
+//         case 2: answer += 'ikki'; break;
+//         case 3: answer += 'uch'; break;
+//         case 4: answer += 'to\'rt'; break;
+//         case 5: answer += 'besh'; break;
+//         case 6: answer += 'olti'; break;
+//         case 7: answer += 'yetti'; break;
+//         case 8: answer += 'sakkiz'; break;
+//         case 9: answer += 'to\'qqiz'; break;
+//         case 0: break;
+//     }
+//     console.log(answer);
+// } else {
+//     console.log('I dont read this old!');
+// }
 
 
 // =============== Switch case 17 ===============
-
+// let number = 20;
+//
+// if(20 <= number && number <= 40) {
+//     let numberTenth = Math.floor(number % 100 / 10);
+//     let numberOneth = number % 10;
+//     let answer;
+//     switch (numberTenth) {
+//         case 2: answer = 'Yigirma '; break;
+//         case 3: answer = 'O\'ttiz '; break;
+//         case 4: answer = 'Qirq '; break;
+//     }
+//     switch (numberOneth) {
+//         case 1: answer += 'bir'; break;
+//         case 2: answer += 'ikki'; break;
+//         case 3: answer += 'uch'; break;
+//         case 4: answer += 'to\'rt'; break;
+//         case 5: answer += 'besh'; break;
+//         case 6: answer += 'olti'; break;
+//         case 7: answer += 'yetti'; break;
+//         case 8: answer += 'sakkiz'; break;
+//         case 9: answer += 'to\'qqiz'; break;
+//         case 0: break;
+//     }
+//     console.log(answer);
+// } else {
+//     console.log('I dont read this!');
+// }
 
 // =============== Switch case 18 ===============
-
+// let number = 260;
+//
+// if(100 <= number && number <= 999) {
+//     let numberThousand = Math.floor(number % 1000 / 100);
+//     let numberTenth = Math.floor(number % 100 / 10);
+//     let numberOneth = number % 10;
+//     let answer;
+//     switch (numberThousand) {
+//         case 1: answer = 'Biryuz '; break;
+//         case 2: answer = 'Ikkiyuz '; break;
+//         case 3: answer = 'Uchyuz '; break;
+//         case 4: answer = 'To\'rtyuz '; break;
+//         case 5: answer = 'Beshyuz '; break;
+//         case 6: answer = 'Oltiyuz '; break;
+//         case 7: answer = 'Yettiyuz '; break;
+//         case 8: answer = 'Sakkizyuz '; break;
+//         case 9: answer = 'To\'qqizyuz '; break;
+//     }
+//     switch (numberTenth) {
+//         case 1: answer += 'O\'n '; break;
+//         case 2: answer += 'Yigirma '; break;
+//         case 3: answer += 'O\'ttiz '; break;
+//         case 4: answer += 'Qirq '; break;
+//         case 5: answer += 'Ellik '; break;
+//         case 6: answer += 'Oltmish '; break;
+//         case 7: answer += 'Yetmish '; break;
+//         case 8: answer += 'Sakson '; break;
+//         case 9: answer += 'To\'qson '; break;
+//     }
+//     switch (numberOneth) {
+//         case 1: answer += 'bir'; break;
+//         case 2: answer += 'ikki'; break;
+//         case 3: answer += 'uch'; break;
+//         case 4: answer += 'to\'rt'; break;
+//         case 5: answer += 'besh'; break;
+//         case 6: answer += 'olti'; break;
+//         case 7: answer += 'yetti'; break;
+//         case 8: answer += 'sakkiz'; break;
+//         case 9: answer += 'to\'qqiz'; break;
+//         case 0: break;
+//     }
+//     console.log(answer);
+// } else {
+//     console.log('I dont read this number!');
+// }
 
 // =============== Switch case 19 ===============
-
+// let periodYear = 1984;
+// let period= 60;
+// let year = 1984;
+//
+// let diffYear = year - periodYear;
+// let residueOfPeriod = diffYear % period;
+//
+// let wholePart = Math.floor(residueOfPeriod / 12);
+// let residuePart = residueOfPeriod % 12;
+// let answer;
+//
+// switch (wholePart) {
+//     case 0: answer = 'yashil'; break;
+//     case 1: answer = 'yashil'; break;
+//     case 2: answer = 'yashil'; break;
+//     case 3: answer = 'yashil'; break;
+//     case 4: answer = 'yashil'; break;
+// }
+//
+// switch (residuePart) {
+//     case 0: answer += ' sichqon'; break;
+//     case 1: answer += ' sigir'; break;
+//     case 2: answer += ' it'; break;
+//     case 3: answer += ' maymun'; break;
+//     case 4: answer += ' quyon'; break;
+//     case 5: answer += ' qoy'; break;
+//     case 6: answer += ' ayiq'; break;
+//     case 7: answer += ' fil'; break;
+//     case 8: answer += ' yolbars'; break;
+//     case 9: answer += ' tovuq'; break;
+//     case 10: answer += ' olmaxon'; break;
+//     case 11: answer += ' tongiz'; break;
+// }
+// console.log(answer);
 
 // =============== Switch case 20 ===============
+// let D = 15; // kun
+// let M = 3;  // oy
+//
+// let burj = '';
+//
+// switch (M) {
+//     case 1:
+//         burj = (D >= 20) ? "Qovg'a" : "Echki";
+//         break;
+//
+//     case 2:
+//         burj = (D >= 19) ? "Baliq" : "Qovg'a";
+//         break;
+//
+//     case 3:
+//         burj = (D >= 21) ? "Qo'y" : "Baliq";
+//         break;
+//
+//     case 4:
+//         burj = (D >= 20) ? "Buzoq" : "Qo'y";
+//         break;
+//
+//     case 5:
+//         burj = (D >= 21) ? "Egizaklar" : "Buzoq";
+//         break;
+//
+//     case 6:
+//         burj = (D >= 22) ? "Qisqichbaqa" : "Egizaklar";
+//         break;
+//
+//     case 7:
+//         burj = (D >= 23) ? "Arslon" : "Qisqichbaqa";
+//         break;
+//
+//     case 8:
+//         burj = (D >= 23) ? "Parizod" : "Arslon";
+//         break;
+//
+//     case 9:
+//         burj = (D >= 23) ? "Tarozi" : "Parizod";
+//         break;
+//
+//     case 10:
+//         burj = (D >= 23) ? "Chayon" : "Tarozi";
+//         break;
+//
+//     case 11:
+//         burj = (D >= 23) ? "O'qotar" : "Chayon";
+//         break;
+//
+//     case 12:
+//         burj = (D >= 22) ? "Echki" : "O'qotar";
+//         break;
+//
+//     default:
+//         burj = "Noto‘g‘ri oy";
+// }
+//
+// console.log(burj);
