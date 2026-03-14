6949cc0c6fdbf9caedf68cbad0e931fafcece012
mullajonovhikmatullo <hikmatullo.mullajonov@gmail.com>
2026-03-15 01:36:33 +0500

    Simple function answers from 24 to 58;

--- javascripts/fun-simple/index.ts
+++ javascripts/fun-simple/index.ts
@@ -335,20 +335,571 @@
 // console.log(Even(10));
 
 // ==================== FunSimple 25 ====================
-function IsSquare (K: number) {
-
-}
+// function IsSquare(K: number): boolean {
+//     //
+//     let Kbutun = Math.sqrt(K);
+//     return Kbutun === Math.floor(Kbutun);
+// }
+//
+// console.log(IsSquare(16));
 
 // ==================== FunSimple 26 ====================
-
+// function IsPower5(son: number): boolean {
+//     //
+//     let isDegree = true;
+//     while (son > 1) {
+//         if (son % 5 !== 0) {
+//             isDegree = false;
+//             break;
+//         }
+//         son = son / 5;
+//     }
+//     return isDegree;
+// }
+// let result = IsPower5(0);
+// console.log(result);
 
 // ==================== FunSimple 27 ====================
-
+// function IsPowerN (K: number, N: number): boolean {
+//     //
+//     let isPower = false;
+//     if(N === 1) return isPower;
+//
+//     let i = 0;
+//     let tempRes = 1;
+//     while (K > tempRes) {
+//         i++;
+//         tempRes = Math.pow(N, i);
+//         if(tempRes === K) {
+//             isPower = true;
+//             break;
+//         }
+//     }
+//     return isPower;
+// }
+// console.log(IsPowerN(4, 2));
 
 // ==================== FunSimple 28 ====================
-
+// function IsPrime (N: number): boolean {
+//     //
+//     if(N < 1) return false;
+//
+//     let i = 2;
+//     let isPrime = true;
+//     while (Math.sqrt(N) >= i) {
+//         if(N % i === 0) {
+//             isPrime = false;
+//             break;
+//         }
+//         i++;
+//     }
+//     return isPrime;
+// }
+//
+// console.log(IsPrime(10));
 
 // ==================== FunSimple 29 ====================
-
+// function DigitCount(K: number):number {
+//     //
+//     let count = 0;
+//     while (K > 0) {
+//         K = Math.floor(K / 10);
+//         count ++;
+//     }
+//     return count;
+// }
+// console.log(DigitCount(1));
+// console.log(DigitCount(12));
+// console.log(DigitCount(123));
 
 // ==================== FunSimple 30 ====================
+// function DigitN(K: number, N: number): number {
+//     //
+//     let count: number = 0, q: number;
+//     while (K > 0) {
+//         q = K % 10;
+//         K = Math.floor(K / 10);
+//         if(N === count + 1) {
+//             return q;
+//         }
+//         count ++;
+//     }
+// }
+//
+// console.log(DigitN(1987, 1));
+
+// ==================== FunSimple 31 ====================
+// function IsPalindrom(N:number):boolean {
+//     let count = DigitCount(N);
+//     let i = 1;
+//     let isPalind = true;
+//     while (i <= count / 2) {
+//         console.log(DigitN(N, i), DigitN(N, count + 1 - i))
+//         if(DigitN(N, i) !== DigitN(N, count + 1 - i)) {
+//             isPalind = false;
+//             return isPalind;
+//         }
+//         i++;
+//     }
+//     return isPalind;
+// }
+//
+// console.log(IsPalindrom(1011101));
+
+
+// ==================== FunSimple 32 ====================
+// function DegToRad (D: number):number {
+//     //
+//     if(0 < D && D < 360) {
+//         return Number((D * Math.PI / 180).toFixed(2));
+//     }
+//     return 0;
+// }
+//
+// console.log(DegToRad(90));
+// console.log(DegToRad(40));
+// console.log(DegToRad(10));
+
+// ==================== FunSimple 33 ====================
+// function RadToDeg (R: number): number {
+//     //
+//     return Number((R * 180 / Math.PI).toFixed(2));
+// }
+//
+// console.log(RadToDeg(1.57));
+// console.log(RadToDeg(2.43));
+// console.log(RadToDeg(7.19));
+
+// ==================== FunSimple 34 ====================
+// function Fact(N: number): number {
+//     let sumFact = N ? 1 : 0;
+//     let i = 1;
+//     while (i <= N) {
+//         sumFact *= i;
+//         i ++;
+//     }
+//     return sumFact;
+// }
+// console.log(Fact(5));
+
+// ==================== FunSimple 35 ====================
+// function Fact2(N: number): number {
+//     //
+//     let sumFact2 = 1;
+//     while(N > 1) {
+//         sumFact2 *= N;
+//         N-=2;
+//     }
+//     return sumFact2;
+// }
+//
+// console.log(Fact2(5));
+
+// ==================== FunSimple 36 ====================
+// function Fib(N: number): number {
+//     let had:number = 1;
+//     let nextHad:number = 1;
+//     let nextNextHad: number = had + nextHad;
+//     let i:number = 4;
+//     while (i <= N) {
+//         had = nextHad;
+//         nextHad = nextNextHad;
+//         nextNextHad = had + nextNextHad;
+//         i ++;
+//     }
+//     return N < 3 ? 1 : nextNextHad;
+// }
+// console.log(Fib(6));
+
+// ==================== FunSimple 37 ====================
+// function Power(A: number, B: number):number {
+//     let result = 1;
+//     let i = 1;
+//     while (i <= B) {
+//         result *= A;
+//         i++;
+//     }
+//     return result;
+// }
+// let res = Power(2, 5);
+// console.log(res);
+
+// ==================== FunSimple 38 ====================
+// function Power2(A: number, N: number): number {
+//     let result = 1;
+//     let i = 1;
+//     while (i <= Math.abs(N)) {
+//         result *= A;
+//         i++;
+//     }
+//
+//     return N < 0 ? 1 / result : result;
+// }
+// let res = Power2(5, 0);
+// console.log(res);
+// res = Power2(2, -1);
+// console.log(res);
+// res = Power2(5, 2);
+// console.log(res);
+
+// ==================== FunSimple 39 ====================
+// function Power3 (A:number, N:number): number {
+//     //
+//     return A - Math.floor(A) ? Power(A, N) : Power2(A, N);
+// }
+// console.log(Power3(2.1, 3))
+
+// ==================== FunSimple 40 ====================
+// function Exp1(x: number, e: number): number {
+//     //
+//     let sum: number = 1;
+//     let sumFact: number = 1;
+//
+//     if(x <= 0 || e <= 1) return -1;
+//
+//     let i: number = 1;
+//     let had: number;
+//
+//     while (true) {
+//         sumFact *= i;
+//         had = Math.pow(x, i) / sumFact;
+//         if (had < e) break;
+//         sum += had;
+//         i++;
+//     }
+//     return sum;
+// }
+//
+// console.log(Exp1(2, 2));
+
+
+// ==================== FunSimple 41 ====================
+// function factorial(n: number) {
+//     let sum = 1, i = 1;
+//     while (i <= n) {
+//         sum *= i;
+//         i ++;
+//     }
+//     return sum;
+// }
+//
+// function sin1(x: number, e: number): number {
+//     //
+//     let sum: number = 1;
+//     let sumFact: number = 1;
+//
+//     if (x <= 0 || e <= 1) return -1;
+//
+//     let i: number = 0;
+//     let had: number;
+//
+//     while (true) {
+//         sumFact *= i;
+//         had = Math.pow(-1, i) * Math.pow(x, 2 * i + 1) / factorial(2 * i + 1);
+//         if (Math.abs(had) < e) break;
+//         sum += had;
+//         i++;
+//     }
+//     return sum;
+// }
+//
+// console.log(sin1(2, 5));
+
+// ==================== FunSimple 42 ====================
+// function factorial(n: number) {
+//     let sum = 1, i = 1;
+//     while (i <= n) {
+//         sum *= i;
+//         i ++;
+//     }
+//     return sum;
+// }
+//
+// function cos1(x: number, e: number): number {
+//     //
+//     let sum: number = 1;
+//     let sumFact: number = 1;
+//
+//     if (x <= 0 || e <= 1) return -1;
+//
+//     let i: number = 0;
+//     let had: number;
+//
+//     while (true) {
+//         sumFact *= i;
+//         had = Math.pow(-1, i) * Math.pow(x, 2 * i) / factorial(2 * i);
+//         if (Math.abs(had) < e) break;
+//         sum += had;
+//         i++;
+//     }
+//     return sum;
+// }
+//
+// console.log(cos1(2, 5));
+
+// ==================== FunSimple 43 ====================
+// function factorial(n: number) {
+//     let sum = 1, i = 1;
+//     while (i <= n) {
+//         sum *= i;
+//         i ++;
+//     }
+//     return sum;
+// }
+//
+// function Ln1(x: number, e: number): number {
+//     //
+//     let sum: number = 1;
+//     let sumFact: number = 1;
+//
+//     if (x <= 0 || e <= 1) return -1;
+//
+//     let i: number = 0;
+//     let had: number;
+//
+//     while (true) {
+//         sumFact *= i;
+//         had = Math.pow(-1, i) * Math.pow(x, i + 1) / factorial(i + 1);
+//         if (Math.abs(had) < e) break;
+//         sum += had;
+//         i++;
+//     }
+//     return sum;
+// }
+//
+// console.log(Ln1(2, 5));
+
+// ==================== FunSimple 44 ====================
+// function Ln1(x: number, e: number): number {
+//     //
+//     let sum: number = 1;
+//     let sumFact: number = 1;
+//
+//     if (x <= 0 || e <= 1) return -1;
+//
+//     let i: number = 0;
+//     let had: number;
+//
+//     while (true) {
+//         sumFact *= i;
+//         had = Math.pow(-1, i) * Math.pow(x, 2*i + 1) / 2 * i + 1;
+//         if (Math.abs(had) < e) break;
+//         sum += had;
+//         i++;
+//     }
+//     return sum;
+// }
+//
+// console.log(Ln1(2, 5));
+
+// ==================== FunSimple 45 ====================
+// function factorial(n: number): number {
+//     let sum = 1, i = 1;
+//     while (i <= n) {
+//         sum *= i;
+//         i++;
+//     }
+//     return sum;
+// }
+//
+// function frontSum(a: number, n: number): number {
+//     //
+//     let sum = 1;
+//     let i = 0;
+//     while (i < n) {
+//         //
+//         sum *= a * (a - i);
+//         i++;
+//     }
+//     return sum;
+// }
+//
+// function Power4(x: number, a: number, e: number): number {
+//     //
+//     let sum = 1;
+//     let i = 0;
+//     let had: number;
+//
+//     while (true) {
+//         //
+//         had = frontSum(a, i) * Math.pow(x, i) / factorial(i);
+//         if (Math.abs(had) < e) return sum;
+//         sum += had;
+//     }
+// }
+//
+// console.log(Power4(3, 1, 3))
+
+// ==================== FunSimple 46 ====================
+// function EKUB(A:number, B:number):number {
+// //
+//     let i = 1;
+//     let min = A > B ? B : A;
+//     let ekub:number = i;
+//     while (i <= min) {
+//         //
+//         i++;
+//         if(A % i === 0 && B % i === 0) {
+//             ekub = i;
+//         }
+//     }
+//     return ekub;
+// }
+//
+// let A = 10;
+// let B = 10;
+// console.log(EKUB(A, B));
+
+// ==================== FunSimple 47 ====================
+// function Frac1(a: number, b: number):string {
+//     let ekub = EKUB(a, b);
+//     a = a / ekub;
+//     b = b / ekub;
+//     return `${a} / ${b}`;
+// }
+//
+// console.log(Frac1(15, 12));
+
+// ==================== FunSimple 48 ====================
+// function EKUK(a:number, b:number):number {
+//     //
+//     let ekub = EKUB(a, b);
+//     a = a / ekub;
+//     b = b / ekub;
+//     let ekuk = a * b * ekub;
+//     return ekuk;
+// }
+//
+// console.log(EKUK(15, 12));
+
+// ==================== FunSimple 49 ====================
+// function EKUB3(A:number, B:number, C:number):number {
+//     //
+//     let ekubAB = EKUB(A, B);
+//     let ekubAC = EKUB(A, C);
+//     let ekubBC = EKUB(B, C);
+//
+//     let mainEkub = Math.min(ekubAB, ekubAC, ekubBC);
+//     console.log(mainEkub);
+// }
+//
+// EKUB3(15, 12, 3);
+
+// ==================== FunSimple 50 ====================
+// function TimeToHMS (time:number):string {
+//     //
+// let hour: string | number = Math.floor(time / 3600);
+// let min: string | number = Math.floor(time % 3600 / 60);
+// let sec:string | number = time % 60;
+//
+//     sec = sec > 9 ? sec : '0' + sec;
+//     min = min > 9 ? min : '0' + min;
+//     hour = hour > 9 ? hour : '0' + hour;
+//
+//     return hour + ':' + min + ':' + sec;
+// }
+//
+// console.log(TimeToHMS(800));
+
+// ==================== FunSimple 51 ====================
+// function IncTime (H, M, S, T) {
+//     //
+//     let seconds = 3600 * H + M * 60 + S + T;
+//     let hour: string | number = Math.floor(seconds / 3600);
+//     let min: string | number = Math.floor(seconds % 3600 / 60);
+//     let sec:string | number = seconds % 60;
+//
+//     sec = sec > 9 ? sec : '0' + sec;
+//     min = min > 9 ? min : '0' + min;
+//     hour = hour > 9 ? hour : '0' + hour;
+//     return hour + ':' + min + ':' + sec;
+// }
+//
+// console.log(IncTime(3, 20, 15, 45));
+
+// ==================== FunSimple 52 ====================
+// function IsLeapYear(Y: number): boolean {
+// //
+//     let isKabisaYear = (Y % 4 === 0 && Y % 100 !== 0) || (Y % 400 === 0);
+//     return isKabisaYear;
+// }
+
+//
+// console.log(IsLeapYear(1600));
+
+// ==================== FunSimple 53 ====================
+// function MonthDays(M: number, Y: number): number {
+//     //
+//     let isCabisa = IsLeapYear(Y);
+//     switch (M) {
+//         case 1:
+//         case 3:
+//         case 5:
+//         case 7:
+//         case 8:
+//         case 10:
+//         case 12:
+//             return 31;
+//         case 4:
+//         case 6:
+//         case 9:
+//         case 11:
+//             return 30;
+//         case 2:
+//             return isCabisa ? 29 : 28;
+//     }
+// }
+
+// MonthDays(2, 1200);
+
+// ==================== FunSimple 54 ====================
+// function PrevDate(D, M, Y) {
+// //
+//     if(D === 1) {
+//         return MonthDays(M === 1 ? 12 : M - 1, Y);
+//     } else {
+//         return D - 1;
+//     }
+// }
+
+// console.log(PrevDate(1, 3, 2000));
+
+// ==================== FunSimple 55 ====================
+// function NextDate(D, M, Y) {
+// //
+//     let days = MonthDays(M, Y);
+//     if(D < days) return D + 1;
+//     else return 1;
+// }
+//
+// console.log(NextDate(28, 2, 2000));
+
+// ==================== FunSimple 56 ====================
+// function Leng(x1, y1, x2, y2) {
+//     //
+//     return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+// }
+//
+// console.log(Leng(5, 3, 9, 6));
+
+// ==================== FunSimple 57 ====================
+// function Perim(x1, y1, x2, y2, x3, y3) {
+//     let ab = Leng(x1, y1, x2, y2);
+//     let ac = Leng(x1, y1, x3, y3);
+//     let bc = Leng(x2, y2, x3, y3);
+//     return ab + ac + bc;
+// }
+
+// ==================== FunSimple 58 ====================
+// function Area(x1, y1, x2, y2, x3, y3) {
+//     //
+//     let ab = Leng(x1, y1, x2, y2);
+//     let ac = Leng(x1, y1, x3, y3);
+//     let bc = Leng(x2, y2, x3, y3);
+//     let p = Perim(x1, y1, x2, y2, x3, y3) / 2;
+//     return Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
+// }
+
+// ==================== FunSimple 59 ====================
+
+// ==================== FunSimple 60 ====================
